f59b2dd32a2769161efe522da24b410c0db00759
aki4110 <may.may.cat@outlook.jp>
2024-02-23 00:44:12 +0900

    ros-testのdockerfileを変更

--- ros/test/setup/setup.bash
+++ ros/test/setup/setup.bash
@@ -1,1 +1,7 @@
-docker run --volume /home/sakuramoto/home_workspace/ros/test/workspace:/app/workspace -it ros-test
+docker run -it --rm --privileged \
+    -e DISPLAY=$DISPLAY \
+    -e XDG_RUNTIME_DIR=/tmp/runtime-root \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /home/sakuramoto/home_workspace/ros/test/workspace:/app/workspace \
+    ros-test
+
